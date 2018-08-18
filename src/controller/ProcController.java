ce67b16131fad7b71d8b5eca0c1b446733d56ed9
BRUNOFERREIRACARDOSO <BRUNOFERREIRACARDOSO@DESKTOP-PAFLB4F>
2018-08-18 18:16:29 -0300

    PROG02 - MUDANÇAS NA CLASSE PROCCONTROLLER

--- src/controller/ProcController.java
+++ src/controller/ProcController.java
@@ -15,10 +15,10 @@ public class ProcController {
 	public void processoCompleto(String so) {
 		if(so.contains("Windows")) {
 			processoLista("TASKLIST");
-			processoWindows();
+			mataProcessoWindows();
 		}else if(so.contains("Linux")) {
-			processoLista("top");
-			processoLinux();
+			processoLista("ps aux");
+			mataProcessoLinux();
 		}
 	}
 	public void processoLista(String path) {
@@ -40,10 +40,74 @@ public class ProcController {
 		}
 		
 	}
-	public void processoWindows() {
-		
+	public void mataProcessoWindows(){
+		String infoProcesso = "";
+		infoProcesso = JOptionPane.showInputDialog("Digite o processo que deseja matar");
+		String cmdPid = "TASKKILL /PID ";
+		String cmdNome = "TASKKILL /IM ";
+		int pid = 0;
+		StringBuffer buffer = new StringBuffer();
+		try{
+			pid = Integer.parseInt(infoProcesso); //ele tenta converter pra inteiro, se não der ele vai pro catch
+			buffer.append(cmdPid);
+			buffer.append(pid);
+		}catch(NumberFormatException e){ //usando numberformat <---
+			buffer.append(cmdNome);
+			buffer.append(infoProcesso);
+		}
+		chamaProcesso(buffer.toString());
 	}
-	public void processoLinux() {
-		
+	public void chamaProcesso(String path){
+		try {
+			Runtime.getRuntime().exec(path);
+		} catch (IOException e) {
+			String erro = e.getMessage();
+			if(erro.contains("740")){ //caso a exception contenha o erro 740(autenticação);
+				StringBuffer buffer = new StringBuffer();
+				buffer.append("cmd /c ");
+				buffer.append(path); //aqui ele adiciona o comando de validação e em seguida ja tenta forçar uma nova run do processo
+				try {
+					Runtime.getRuntime().exec(buffer.toString());
+				} catch (IOException e1) { // e1 pq a exception esta dentro de uma exception
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			else{
+				e.printStackTrace();
+			}
+		}
 	}
+	public void mataProcessoLinux() {
+		String infoProcesso = "";
+		infoProcesso = JOptionPane.showInputDialog("Digite o processo que deseja matar");
+		String cmdPid = "kill ";
+		String cmdNome = "killall ";
+		int pid = 0;
+		StringBuffer buffer = new StringBuffer();
+		try{
+			pid = Integer.parseInt(infoProcesso); //ele tenta converter pra inteiro, se não der ele vai pro catch
+			buffer.append(cmdPid);
+			buffer.append(pid);
+		}catch(NumberFormatException e){ //usando numberformat <---
+			buffer.append(cmdNome);
+			buffer.append(infoProcesso);
+		}
+		try {
+			Runtime.getRuntime().exec(buffer.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+	}
+	
 }
+	/*public String validaProcesso(String infoProcesso) {
+		do {
+			infoProcesso = JOptionPane.showInputDialog("Digite o valor ou o pid do processo");
+		}while(!(infoProcesso.contains("exe")) || !(infoProcesso.contains("1")) ||  !(infoProcesso.contains("2")) ||  !(infoProcesso.contains("3"))|| !(infoProcesso.contains("4")) 
+				|| !(infoProcesso.contains("5")) || !(infoProcesso.contains("6")) || !(infoProcesso.contains("7")) || !(infoProcesso.contains("8")) ||!(infoProcesso.contains("9"))
+				|| !(infoProcesso.contains("10")));
+		return infoProcesso;
+	}*/
+
