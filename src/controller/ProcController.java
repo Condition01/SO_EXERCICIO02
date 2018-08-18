6d8d4033283b92c9f1745c5d387157592b64b203
BRUNOFERREIRACARDOSO <BRUNOFERREIRACARDOSO@DESKTOP-PAFLB4F>
2018-08-18 17:26:54 -0300

    PROG02

--- src/controller/ProcController.java
+++ src/controller/ProcController.java
@@ -1,5 +1,49 @@
 package controller;
 
-public class ProcController {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
+import javax.swing.JOptionPane;
+import javax.swing.plaf.SliderUI;
+
+public class ProcController {
+	public String so() {
+		return System.getProperty("os.name");
+	}
+	public void processoCompleto(String so) {
+		if(so.contains("Windows")) {
+			processoLista("TASKLIST");
+			processoWindows();
+		}else if(so.contains("Linux")) {
+			processoLista("top");
+			processoLinux();
+		}
+	}
+	public void processoLista(String path) {
+		try {			
+			Process proc = Runtime.getRuntime().exec(path);
+			InputStream fluxo = proc.getInputStream(); // fluxo recebe stream de proc
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+			String linha = buffer.readLine();
+			while(linha != null){ //o laço vai até a ultima linha
+				System.out.println(linha);
+				linha = buffer.readLine(); //atribuindo linha com a proxima linha do arquivo
+			}
+		} catch (IOException e){
+			String erro = e.getMessage();
+			JOptionPane.showMessageDialog(null, erro, "ERRO", JOptionPane.ERROR_MESSAGE); //CONSTANTE QUE EXISTE DENTRO DA CLASSE JOPTIONPANE, O "ERRO" VAI APARECER NO CABEÇALHO
+			e.printStackTrace(); //erro aparece no console
+			
+		}
+		
+	}
+	public void processoWindows() {
+		
+	}
+	public void processoLinux() {
+		
+	}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,18 @@
 package view;
 
-public class Main {
+import javax.swing.JOptionPane;
 
+public class Main {
+	public static void main(String[] args) {
+		int opc;
+		do {
+			opc = Integer.parseInt(JOptionPane.showInputDialog("Digite 1 para listar os processos e mata-los" + "\nDigite 9 para sair do programa"));
+			switch(opc) {
+			case 1:	
+				break;
+			case 2: System.exit(0);
+				break;
+			}
+		}while(opc!=9);
+	}
 }
